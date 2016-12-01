0404707bf68ac1b1f87f8e954ef126dba50181db
Vincent de RIBOU <v.deribou@gmail.com>
2016-12-01 08:44:56 +0100

    If no preferred libray format is set, use static format and inform about it

--- comps/comp2/comp2_sub1/test/src.cmake
+++ comps/comp2/comp2_sub1/test/src.cmake
@@ -5,6 +5,6 @@ set(COMP_SRC_PROFILE "test")
 set(COMP_SRC_TEST_NAME "comp2s2ss1")
 
 set(COMP_SRC_LIB_DEPS "comp2s1")
-set(COMP_SRC_DEPS_PREFFERED "shared")
+set(COMP_SRC_DEPS_PREFERRED "shared")
 
 set(LOCAL_SRC_FILES main.c)
--- comps/comp2/comp2_sub2/comp2_sub2_ssub1/src/src.cmake
+++ comps/comp2/comp2_sub2/comp2_sub2_ssub1/src/src.cmake
@@ -5,7 +5,7 @@ set(COMP_SRC_PROFILE "application")
 set(COMP_SRC_APP_NAME "comp2s2ss1")
 
 set(COMP_SRC_LIB_DEPS "comp2s1")
-set(COMP_SRC_DEPS_PREFFERED "static")
+# set(COMP_SRC_DEPS_PREFERRED "static")
 
 set(LOCAL_SRC_FILES 
     comp2s2_ss1
